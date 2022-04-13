17a9ed1706d102ed9db57fc6dc8da24605a63d8f
Sagar Chougule <92chougulesagar@gmail.com>
2022-04-13 11:01:31 +0530

    Fill Mapping properties

---
+++ ArcPy.Mapping.py
@@ -0,0 +1,14 @@
+import arcpy
+from arcpy import env
+env.workspace = r'D:\Other\Practice'
+mxd = arcpy.mapping.MapDocument('Current')
+mxd.author = "Sagar Chougule"
+mxd.tito
+mxd.title = "Electricity Mapping"
+mxd.description = "The Python programming for analysis and mapping"
+mxd.summary = "Python and ArcGIS"
+mxd.credits = "KSRDPR University"
+mxd.tags = "ArcGIS, QGIS, Python, ArcPy, Geospatial"
+mxd.hyperlinkBase = "https://github.com/92sagarchougule"
+mxd.save()
+
