c7c71c40defcc40ac92881b27c2adb1823d27e4d
Sagar Chougule <92chougulesagar@gmail.com>
2022-04-13 11:24:53 +0530

    Added new properties

--- ArcPy.Mapping.py
+++ ArcPy.Mapping.py
@@ -3,6 +3,7 @@ from arcpy import env
 env.workspace = r'D:\Other\Practice'
 mxd = arcpy.mapping.MapDocument('Current')
 mxd.author = "Sagar Chougule"
+mxd.tito
 mxd.title = "Electricity Mapping"
 mxd.description = "The Python programming for analysis and mapping"
 mxd.summary = "Python and ArcGIS"
@@ -10,4 +11,18 @@ mxd.credits = "KSRDPR University"
 mxd.tags = "ArcGIS, QGIS, Python, ArcPy, Geospatial"
 mxd.hyperlinkBase = "https://github.com/92sagarchougule"
 mxd.save()
+mxd.relativePaths = True
+mxd.relativePaths = False
+mxd.relativePaths = True
+mxd.makeThumbnail()
+mxd.save()
+datesave = mxd.dateSaved()
+datesave = mxd.dateSaved
+type(datesave)
+datesave.date
+datesave.date()
+datesave.day
+datesave.date
+datesave.time
+datesave.time()
 
