ee227a759ca2d751e0268515112b406cc634c16a
Sagar Chougule <92chougulesagar@gmail.com>
2022-04-13 11:59:10 +0530

    add new properties

--- ArcPy.Mapping.py
+++ ArcPy.Mapping.py
@@ -3,7 +3,6 @@ from arcpy import env
 env.workspace = r'D:\Other\Practice'
 mxd = arcpy.mapping.MapDocument('Current')
 mxd.author = "Sagar Chougule"
-mxd.tito
 mxd.title = "Electricity Mapping"
 mxd.description = "The Python programming for analysis and mapping"
 mxd.summary = "Python and ArcGIS"
@@ -12,17 +11,12 @@ mxd.tags = "ArcGIS, QGIS, Python, ArcPy, Geospatial"
 mxd.hyperlinkBase = "https://github.com/92sagarchougule"
 mxd.save()
 mxd.relativePaths = True
-mxd.relativePaths = False
-mxd.relativePaths = True
 mxd.makeThumbnail()
-mxd.save()
-datesave = mxd.dateSaved()
 datesave = mxd.dateSaved
 type(datesave)
-datesave.date
 datesave.date()
 datesave.day
 datesave.date
 datesave.time
-datesave.time()
-
+dataFrame = mxd.activeDataFrame
+arcpy.RefreshTOC()
