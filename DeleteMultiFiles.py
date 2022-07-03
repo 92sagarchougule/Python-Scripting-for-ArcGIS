b818878d270e7f2d3a93a78e5548805d302f0581
Sagar Chougule <92chougulesagar@gmail.com>
2022-07-03 20:45:33 +0530

    Delete multiple files in arccatlog

---
+++ DeleteMultiFiles.py
@@ -0,0 +1,25 @@
+help(arcpy)
+
+arcpy.env.workspace = r'D:\Delete Data'
+fclist = arcpy.ListFeatureClasses()
+for i in fclist:
+    print(i)
+    
+for s in fclist:
+    arcpy.DeleteFeatures_management(s)
+    
+for s in fclist:
+    print(s)
+    
+for k in fclist:
+    arcpy.Delete_management(k)
+    
+rslist = arcpy.ListRasters()
+for rsl in rslist:
+    print(rsl)
+    
+len(rslist)
+for rl in rslist:
+    arcpy.Delete_management(rl)
+    
+
