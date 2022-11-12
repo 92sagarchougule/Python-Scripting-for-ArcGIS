abf075efdcca354c7b7c634d9b5636de5da0b316
Sagar Chougule <92chougulesagar@gmail.com>
2022-11-12 19:08:01 +0530

    calculate length

---
+++ Calculate_Length.py
@@ -0,0 +1,19 @@
+#Calculate_Area.py
+#Developer: Sagar Chougule
+#Tool to calculate Area
+
+import arcpy
+
+shp = arcpy.GetParameterAsText(0)
+
+field = arcpy.GetParameterAsText(1)
+
+query = '!shape.length!'
+
+arcpy.CalculateField_management(shp, field, query,"PYTHON")
+
+arcpy.AddMessage('\n')
+arcpy.AddMessage('Done')
+arcpy.AddMessage('\n')
+
+
